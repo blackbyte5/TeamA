98658cdd03aedb55db928478b30ce6c2646f0eff
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-15 06:10:04 +0800

    Add files via upload

--- add_download_COD.php
+++ add_download_COD.php
@@ -17,7 +17,7 @@ include 'navs/admin_navs.php';
      if (isset($_POST['add'])) {
 
        if (!empty($_FILES['pdf_file']['name'])) {
-       $targetDir = '/upload/master/downloadable';
+       $targetDir = 'downloadable/';
        $Requirements=basename($_FILES['pdf_file']['name']);
        $targetFilePath = $targetDir . $Requirements;
        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
