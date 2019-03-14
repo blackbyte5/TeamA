7743f86c266be8a61166c6c032c209d25f1616c1
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-14 18:03:52 +0800

    Add files via upload

--- server.php
+++ server.php
@@ -2,7 +2,7 @@
 
 session_start();
 // Create connection
-$conn = mysqli_connect("localhost", "root", "");
+$conn = mysqli_connect("sql12283241", "sql12283241", "nUZmbYZs5G");
 
 $db = mysqli_select_db($conn,"iserve");
 
