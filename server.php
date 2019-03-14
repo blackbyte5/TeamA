1c96e6ca98b5b375814ac284996821364ea8311e
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-14 18:15:42 +0800

    Add files via upload

--- server.php
+++ server.php
@@ -2,9 +2,9 @@
 
 session_start();
 // Create connection
-$conn = mysqli_connect('sql12.freemysqlhosting.net', 'sql12283241', 'nUZmbYZs5G');
+$conn = mysqli_connect("sql12.freemysqlhosting.net:3306", "sql12283241", "nUZmbYZs5G");
 
-$db = mysqli_select_db($conn,'sql12283241');
+$db = mysqli_select_db($conn,"sql12283241");
 
 
 ?>
