dcb0f42cfec379581fa6633457a7dadc4eed6533
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-15 05:46:59 +0800

    Add files via upload

--- add_download_COD.php
+++ add_download_COD.php
@@ -17,7 +17,7 @@ include 'navs/admin_navs.php';
      if (isset($_POST['add'])) {
 
        if (!empty($_FILES['pdf_file']['name'])) {
-       $targetDir = 'https://github.com/blackbyte5/TeamA/upload/master/downloadable';
+       $targetDir = 'blackbyte5/TeamA/upload/master/downloadable';
        $Requirements=basename($_FILES['pdf_file']['name']);
        $targetFilePath = $targetDir . $Requirements;
        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
