7477edccbfa58da9bd8749e2cfa1a6304518ab30
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-16 05:32:25 +0800

    Update senior_applicants_approved_details.php

--- senior_applicants_approved_details.php
+++ senior_applicants_approved_details.php
@@ -135,7 +135,7 @@
       echo "Invalid age for Senior Citizen";
     }
     elseif (empty($userid)) {
-      echo (mysqli_error($conn))."Session Timeout <br> Make Sure You're not loging in deffirent account";
+      echo (mysqli_error($conn))."Session Timeout <br> Make Sure You're not login deffirent account";
     }
 else {
 
