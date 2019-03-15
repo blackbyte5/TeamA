d65315c6614be3ffe1db76767b54f0d57fa405ab
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-16 01:23:51 +0800

    Add files via upload

--- edit_emp.php
+++ edit_emp.php
@@ -109,6 +109,7 @@ include 'navs/admin_navs.php';
           <div class="card-body">
             <div class="table-responsive">
               <table class="table table-bordered" id="customers" width="100%" cellspacing="0">
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Employee Number..">
                 <thead>
                   <tr>
                     <th>Employee ID</th>
@@ -152,3 +153,26 @@ include 'navs/admin_navs.php';
 
 </body>
 </html>
+<script>
+function myFunction() {
+// Declare variables
+var input, filter, table, tr, td, i, txtValue;
+input = document.getElementById("myInput");
+filter = input.value.toUpperCase();
+table = document.getElementById("customers");
+tr = table.getElementsByTagName("tr");
+
+// Loop through all table rows, and hide those who don't match the search query
+for (i = 0; i < tr.length; i++) {
+  td = tr[i].getElementsByTagName("td")[0];
+  if (td) {
+    txtValue = td.textContent || td.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
+}
+</script>
