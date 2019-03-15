d215985c1834cec231ffa28cea9961eb74c1d064
blackbyte5 <42024280+blackbyte5@users.noreply.github.com>
2019-03-16 05:31:39 +0800

    Update senior_applicants_details.php

--- senior_applicants_details.php
+++ senior_applicants_details.php
@@ -132,7 +132,7 @@
       echo (mysqli_error($conn))."Fill-up all required fields <a style='color:black;'>( * )</a> <br>or<br> Attach your Requirements..";
     }
     elseif (empty($userid)) {
-      echo (mysqli_error($conn))."Session Timeout <br> Make Sure You're not loging in deffirent account";
+      echo (mysqli_error($conn))."Session Timeout <br> Make Sure You're not login deffirent account";
     }
 
     elseif ($age < 60){
